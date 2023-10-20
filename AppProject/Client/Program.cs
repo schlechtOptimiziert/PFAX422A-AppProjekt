b7b58cf61bc94e9f90cb7badba3e62a4dfec42ef
Steinkuhle, Amelie <ast@connext.de>
2023-10-20 17:09:31 +0200

    MudBlazor zum Client hinzugefügt mainpae umbenannt

--- AppProject/Client/Program.cs
+++ AppProject/Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AppProject.Client;
+using MudBlazor.Services;
+
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();

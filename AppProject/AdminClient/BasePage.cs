a7f2dc97c956ecde5d5bd151e1c7430a5827dcfb
Dominik von Haller <vonhaller@fastec.de>
2023-10-20 16:15:44 +0200

    Add MudBlazor to AdminClient

---
+++ AppProject/AdminClient/BasePage.cs
@@ -0,0 +1,20 @@
+﻿//using Client.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace AppProject.AdminClient;
+
+public abstract class BasePage : ComponentBase
+{
+    private readonly CancellationTokenSource cancellationTokenSource = new();
+    protected CancellationToken CancellationToken { get; }
+
+    //[Inject]
+    //public IService Service { get; set; }
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    public bool IsLoading { get; set; }
+    public bool IsCreate { get; set; }
+
+    protected BasePage() => CancellationToken = cancellationTokenSource.Token;
+}

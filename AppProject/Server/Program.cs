e10b56bd1bf123cb3504cdc271c51fffdbd830ae
schlechtOptimiziert <107460536+schlechtOptimiziert@users.noreply.github.com>
2023-10-30 14:27:47 +0100

    Add Swagger (#4)

--- AppProject/Server/Program.cs
+++ AppProject/Server/Program.cs
@@ -2,8 +2,6 @@ using AppProject.Server.EntityModel;
 using AppProject.Server.EntityModel.Repositories;
 using AppProject.Server.EntityModel.Repositories.Interfaces;
 using AppProject.Server.Settings;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +19,15 @@ builder.Services.AddSqlServer<AppProjectDbContext>(serviceConfig.ConnectionStrin
 
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blazor API V1");
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

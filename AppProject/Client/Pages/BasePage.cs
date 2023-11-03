3f54783e0c8bb6893eacd3e2aa79a5ca9a7120c5
schlechtOptimiziert <107460536+schlechtOptimiziert@users.noreply.github.com>
2023-11-03 13:26:36 +0100

    Features/add item details to client (#5)

---
+++ AppProject/Client/Pages/BasePage.cs
@@ -0,0 +1,8 @@
+﻿using AppProject.Client.Components;
+using AppProject.ServerDataProvider.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace AppProject.Client.Pages;
+
+public class BasePage : BaseComponent
+{ }
---
+++ AppProject/Client/Pages/ItemDetails.razor.cs
@@ -0,0 +1,27 @@
+﻿using AppProject.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace AppProject.Client.Pages;
+
+partial class ItemDetails : BasePage
+{
+    private Item item;
+
+    [Parameter]
+    public long? Id { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsLoading = true;
+        await base.OnInitializedAsync().ConfigureAwait(false);
+        item = await GetItemAsync().ConfigureAwait(false);
+        IsLoading = false;
+    }
+
+    private async Task<Item> GetItemAsync()
+    {
+        if (Id.HasValue)
+            return await Service.GetItemAsync(Id.Value, CancellationToken).ConfigureAwait(false) ?? null;
+        return null;
+    }
+}

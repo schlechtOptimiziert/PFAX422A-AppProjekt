2ca9073d9731673298f228906df4462c2c6a8452
schlechtOptimiziert <dominikvonhaller@gmail.com>
2023-11-04 15:13:57 +0100

    Fix remark

---
+++ AppProject/Client/PictureConverter.cs
@@ -0,0 +1,9 @@
+﻿using AppProject.Shared;
+
+namespace AppProject.Client;
+
+public static class PictureConverter
+{
+    public static string ItemPictureToUri(ItemPicture picture)
+        => string.Format("data:image/png;base64,{0}", Convert.ToBase64String(picture.Bytes));
+}

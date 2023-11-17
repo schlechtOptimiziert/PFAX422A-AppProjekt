7443095fb732bf3920e8285a970bba0706f2261f
Dominik von Haller <vonhaller@fastec.de>
2023-11-17 14:06:34 +0100

    Add redirect

--- MobileClient/Client/Components/SearchBar.razor.cs
+++ MobileClient/Client/Components/SearchBar.razor.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using ClientComponents.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace MobileClient.Client.Components;
 
---
+++ MobileClient/Client/Shared/RedirectToLogin.razor.cs
@@ -0,0 +1,16 @@
+﻿using ClientComponents.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace MobileClient.Client.Shared;
+
+public partial class RedirectToLogin : BaseComponent
+{
+    [Inject]
+    private NavigationManager Navigation { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Navigation.NavigateToLogin("authentication/login");
+    }
+}

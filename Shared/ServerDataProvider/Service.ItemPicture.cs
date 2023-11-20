3dd261551d9490330cfdb4276c26252c4af413af
Oliver Ruda <o.ruda@fastec.de>
2023-11-20 10:12:15 +0100

    fixed dum dum mistake

--- Shared/ServerDataProvider/Service.ItemPicture.cs
+++ Shared/ServerDataProvider/Service.ItemPicture.cs
@@ -1,12 +1,5 @@
 ﻿using ServerDataProvider.Helpers;
 using ServerDataProvider.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using TransferModel;
 
 namespace ServerDataProvider;
@@ -15,28 +8,28 @@ public partial class Service : IItemPictureService
 {
     public async Task<long> AddItemPictureAsync(ItemPicture picture, long itemId, CancellationToken cancellationToken)
     {
-        var response = await httpClient.PostAsJsonAsync($"{cartRequestUri}/{itemId}/Pictures", picture, cancellationToken).ConfigureAwait(false);
+        var response = await httpClient.PostAsJsonAsync($"{itemRequestUri}/{itemId}/Pictures", picture, cancellationToken).ConfigureAwait(false);
         response.EnsureSuccessStatusCode();
         return await response.ReadAsAsync<long>(cancellationToken).ConfigureAwait(false);
     }
 
     public async Task<IEnumerable<ItemPicture>> GetItemPicturesAsync(long itemId, CancellationToken cancellationToken)
     {
-        var response = await httpClient.GetAsync($"{cartRequestUri}/{itemId}/Pictures", cancellationToken).ConfigureAwait(false);
+        var response = await httpClient.GetAsync($"{itemRequestUri}/{itemId}/Pictures", cancellationToken).ConfigureAwait(false);
         response.EnsureSuccessStatusCode();
         return await response.ReadAsAsync<IEnumerable<ItemPicture>>(cancellationToken).ConfigureAwait(false);
     }
 
     public async Task<ItemPicture> GetItemCoverPictureAsync(long itemId, CancellationToken cancellationToken)
     {
-        var response = await httpClient.GetAsync($"{cartRequestUri}/{itemId}/Pictures/CoverPicture", cancellationToken).ConfigureAwait(false);
+        var response = await httpClient.GetAsync($"{itemRequestUri}/{itemId}/Pictures/CoverPicture", cancellationToken).ConfigureAwait(false);
         response.EnsureSuccessStatusCode();
         return await response.ReadAsAsync<ItemPicture>(cancellationToken).ConfigureAwait(false);
     }
 
     public async Task DeleteItemPictureAsync(long itemId, long id, CancellationToken cancellationToken)
     {
-        var response = await httpClient.DeleteAsync($"{cartRequestUri}/{itemId}/Pictures/{id}", cancellationToken).ConfigureAwait(false);
+        var response = await httpClient.DeleteAsync($"{itemRequestUri}/{itemId}/Pictures/{id}", cancellationToken).ConfigureAwait(false);
         response.EnsureSuccessStatusCode();
     }
 }

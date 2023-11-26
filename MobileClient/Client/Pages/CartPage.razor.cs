5f26c33eecf7d2778294f67a1fde2b79b9583de7
Oliver Ruda <o.ruda@fastec.de>
2023-11-26 22:15:36 +0100

    fixed no image in cart changed style of keys

--- MobileClient/Client/Pages/CartPage.razor.cs
+++ MobileClient/Client/Pages/CartPage.razor.cs
@@ -24,7 +24,9 @@ public partial class CartPage : BasePage
         foreach (var item in cartItems.Select(c => c.Item))
         {
             var coverPicture = await Service.GetItemCoverPictureAsync(item.Id, CancellationToken).ConfigureAwait(false);
-            item.CoverPictureUri = ItemPicture.ItemPictureToUri(coverPicture);
+
+            if (coverPicture != null)
+                item.CoverPictureUri = ItemPicture.ItemPictureToUri(coverPicture);
         }
     }
 

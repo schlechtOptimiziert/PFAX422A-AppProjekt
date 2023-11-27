c1b9c04b9769a687e1f6e2beff61c1b47880f38f
olr-fh <147176707+olr-fh@users.noreply.github.com>
2023-11-27 11:23:16 +0100

    Gefilterte Sachen richtig machen (#23)

--- MobileClient/Client/Pages/Homepage.razor.cs
+++ MobileClient/Client/Pages/Homepage.razor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using TransferModel;
 
@@ -8,20 +9,32 @@ namespace MobileClient.Client.Pages;
 public partial class Homepage : BasePage
 {
     private IEnumerable<Item> items = Enumerable.Empty<Item>();
+    private IEnumerable<Item> buyAgainItems = Enumerable.Empty<Item>();
     private IEnumerable<Platform> platforms = Enumerable.Empty<Platform>();
 
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync().ConfigureAwait(false);
-        items = await GetItemsAsync().ConfigureAwait(false);
-        platforms = await GetPlatformsAsync().ConfigureAwait(false);
+        items = await GetItemsAsync(CancellationToken).ConfigureAwait(false);
+        buyAgainItems = await GetBuyAgainItemsAsync(CancellationToken).ConfigureAwait(false);
+        platforms = await GetPlatformsAsync(CancellationToken).ConfigureAwait(false);
     }
 
-    private async Task<IEnumerable<Item>> GetItemsAsync()
-        => await Service.GetItemsAsync(CancellationToken).ConfigureAwait(false) ?? Enumerable.Empty<Item>();
+    private async Task<IEnumerable<Item>> GetItemsAsync(CancellationToken cancellationToken)
+        => await Service.GetItemsAsync(cancellationToken).ConfigureAwait(false) ?? Enumerable.Empty<Item>();
 
-    private async Task<IEnumerable<Platform>> GetPlatformsAsync()
-        => await Service.GetPlatformsAsync(CancellationToken).ConfigureAwait(false) ?? Enumerable.Empty<Platform>();
+    private async Task<IEnumerable<Item>> GetBuyAgainItemsAsync(CancellationToken cancellationToken)
+    {
+        var orders = await Service.GetOrdersAsync(UserId, true, cancellationToken).ConfigureAwait(false) ?? Enumerable.Empty<Order>();
+        List<Item> orderItems = new();
+        foreach (var order in orders)
+            if (orderItems.Count <= 7)
+                orderItems = orderItems.Concat(order.Items.DistinctBy(o => o.Id)).ToList();
+        return orderItems.DistinctBy(o => o.Id);
+    }
+
+    private async Task<IEnumerable<Platform>> GetPlatformsAsync(CancellationToken cancellationToken)
+        => await Service.GetPlatformsAsync(cancellationToken).ConfigureAwait(false) ?? Enumerable.Empty<Platform>();
 
     private void NavigateToList()
         => NavigationManager.NavigateTo($"/items");
